ef357ea7eaa135416acb3d2aa3889bef90893eb0
Alexandre Salsinha <alexsalsinha@gmail.com>
2020-03-14 15:37:30 +0000

    Updates

---
+++ precache-manifest.b275446e52991af428e78ff0664f0e63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca75643772932baf00299b7f6c6e4768",
+    "url": "/whattoeat/index.html"
+  },
+  {
+    "revision": "5f1e4d4be2b3e109b212",
+    "url": "/whattoeat/static/css/main.77d389ae.chunk.css"
+  },
+  {
+    "revision": "7e6c89c0d3189c6fe650",
+    "url": "/whattoeat/static/js/2.6802c638.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/whattoeat/static/js/2.6802c638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f1e4d4be2b3e109b212",
+    "url": "/whattoeat/static/js/main.f70fffcd.chunk.js"
+  },
+  {
+    "revision": "dd2e1ce4ef399e8aac28",
+    "url": "/whattoeat/static/js/runtime-main.eb9870c6.js"
+  },
+  {
+    "revision": "6f454a6540390e62e48be686c9e8cbac",
+    "url": "/whattoeat/static/media/not-found.6f454a65.png"
+  },
+  {
+    "revision": "5b15ff76353f20ad8291fc94b4539956",
+    "url": "/whattoeat/static/media/plates-four.5b15ff76.png"
+  }
+]);
